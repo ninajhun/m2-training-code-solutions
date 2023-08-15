fa8429e139ff79ad738e188d57eec836ceb22513
“ninajhun” <ninajhun@berkeley.edu>
2023-08-15 03:13:07 +0000

    completed array-methods

---
+++ array-methods/filter.js
@@ -0,0 +1,21 @@
+const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+
+const names = [
+  'Ada',
+  'Hedy',
+  'Jean',
+  'Grace',
+  'Evelyn',
+  'Joan',
+  'Elizabeth',
+  'Janese',
+  'Donna',
+];
+
+const evenNums = numbers.filter((num) => num % 2 === 0);
+console.log('Even numbers: ', evenNums);
+
+const noDChars = names.filter(
+  (name) => !name.includes('D') && !name.includes('d')
+);
+console.log('No D: ', noDChars);
